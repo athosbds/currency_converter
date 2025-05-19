5707a83508f83b140ae34aadc0284857b2b5c26d
Athos <athosbds@gmail.com>
2025-05-19 18:57:56 -0300

    ad

---
+++ models.py
@@ -0,0 +1,13 @@
+import requests
+
+class CurrencyConverter:
+    def __init__(self, api_urls):
+        self.api_urls = api_urls
+        self.rates = {}
+    def fetch_rates(self):
+        for currency, url in self.api_urls:
+            response = requests.get(url)
+            if response.status_code == 200:
+                self.rates[currency] = response.json().get('rate', {})
+            else:
+                raise Exception(f'Error fethcing')
