795047df953190f51c877c62521a6ec32cf39a4b
Athos <athosbds@gmail.com>
2025-05-27 21:22:53 -0300

    URLS

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request
-
+from models import CurrencyConverter
 app = Flask(__name__)
 
 base_url = 'https://api.exchangerate-api.com/v4/latest/'
 
 API_URLS = {
-    
+    'USD': base_url + 'USD',
+    'GBP': base_url + 'GBP',
+    'EUR': base_url + 'EUR',
 }
+
 
 @app.route('/')
 def index():
