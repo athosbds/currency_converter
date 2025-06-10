e1f95bcbfb8d663e68ef38b843f938d94671b366
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-10 19:59:39 -0300

    ad

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ API_URLS = {
 
 converter = CurrencyConverter(API_URLS)
 
-@app.route('/', method=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     result = None
     error = None
     if request.method == 'POST':
         amount = float(request.form.get('amount', 0))
-        from_currency = request.form.get(from_currency)
-        to_currency = request.form.get(to_currency)
+        from_currency = request.form.get('from_currency')
+        to_currency = request.form.get('to_currency')
         try:
             converter.fetch_rates()
             result = converter.convert(amount, from_currency, to_currency)
--- models.py
+++ models.py
@@ -8,20 +8,27 @@ class CurrencyConverter:
         """
         Aqui busca as taxas de câmbio apartir das 
         URL Api presentes app.py"""
-        for currency, url in self.api_urls:
+        for currency, url in self.api_urls.items():
             response = requests.get(url)
             if response.status_code == 200:
-                self.rates[currency] = response.json().get('rate', {})
+                self.rates[currency] = response.json().get('rates', {})
             else:
                 raise Exception(f'Error fethcing {currency}')  
     def convert(self, amount, from_currency, to_currency):
         """Conversões utilizando o Dólar americano"""
         if from_currency == to_currency:
-            return amount
-        if from_currency == 'USD':
-            base_amount = amount
-        else:
-            base_amount = amount / self.rates['USD'].get(from_currency, 1)
-        if to_currency == 'USD':
-            return base_amount
-        return base_amount * self.rates['USD'].get(to_currency, 1)
+                return round(amount, 2)
+        try:
+            usd_rates = self.rates['USD']
+            if from_currency != 'USD':
+                amount_usd = amount /  usd_rates[from_currency]
+            else:
+               amount_usd = amount
+            if to_currency != 'USD':
+                converter_amount = amount_usd * usd_rates[to_currency]
+            else:
+                converter_amount =  amount_usd
+            return round(converter_amount, 2)
+        except KeyError:
+            raise Exception('Moeda não suportada')
+            
