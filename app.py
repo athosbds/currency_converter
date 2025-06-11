c34fafde3c2a598581e2ba8f478d0a1ceb6496b5
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-11 20:20:17 -0300

    error result

--- app.py
+++ app.py
@@ -1,30 +1,23 @@
 from flask import Flask, render_template, request
 from models import CurrencyConverter
 app = Flask(__name__)
-
-base_url = 'https://api.exchangerate.host/latest?base='
-
-API_URLS = {
-    'USD': base_url + 'USD',
-    'BRL': base_url + 'BRL',
-    'EUR': base_url + 'EUR',
-}
-
-converter = CurrencyConverter(API_URLS)
+converter = CurrencyConverter()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = None
     error = None
-    if request.method == 'POST':
-        amount = float(request.form.get('amount', 0))
-        from_currency = request.form.get('from_currency')
-        to_currency = request.form.get('to_currency')
-        try:
-            converter.fetch_rates()
+    try:
+        if request.method == 'POST':
+            amount = float(request.form.get('amount', 0))
+            from_currency = request.form.get('from_currency')
+            to_currency = request.form.get('to_currency')
             result = converter.convert(amount, from_currency, to_currency)
-        except Exception as e:
-            error = str(e)
+    except ValueError:
+        error = 'Valor Inválido'
+    except Exception as e:
+        error = str(e)
     return render_template('index.html', result=result, error=error)
+
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,29 +1,14 @@
 import requests
 
 class CurrencyConverter:
-    def __init__(self, api_urls):
-        self.api_urls = api_urls
-        self.rates = {}
-    def fetch_rates(self):
-        """
-        Aqui busca as taxas de câmbio apartir das 
-        URL Api presentes app.py"""
-        for currency, url in self.api_urls.items():
-            response = requests.get(url)
-            if response.status_code == 200:
-                self.rates[currency] = response.json().get('rates', {})
-            else:
-                raise Exception(f'Error fethcing {currency}')  
     def convert(self, amount, from_currency, to_currency):
-        """Conversões utilizando o Dólar americano"""
         if from_currency == to_currency:
-                return round(amount, 2)
-        try:
-            from_rates = self.rates[from_currency]
-            if to_currency not in from_rates:
-                raise Exception('Conversão não suportada')
-            converted_amount =  amount * from_rates[to_currency] 
-            return round(converted_amount, 2)
-        except KeyError:
-            raise Exception('Moeda não suportada')           
+            return round(amount, 2)
+        url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}"
+        respo =  requests.get(url)
+        if respo.status_code == 200:
+            data = respo.json()
+            return round(data['result'], 2)
+        else:
+            raise Exception('Erro')          
             
