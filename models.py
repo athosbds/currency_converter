cef33a90ebf9203773b7cd397596fa5ff6b24a76
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-11 21:03:05 -0300

    ad

--- models.py
+++ models.py
@@ -4,11 +4,16 @@ class CurrencyConverter:
     def convert(self, amount, from_currency, to_currency):
         if from_currency == to_currency:
             return round(amount, 2)
-        url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}"
+        url = url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}"
+
         respo =  requests.get(url)
         if respo.status_code == 200:
             data = respo.json()
-            return round(data['result'], 2)
+            print(f'Resposta da API: {data}')     
+            if data.get('success') and 'result' in data:
+                return round(data['result'], 2)
+            else:
+                raise Exception(f'Inválido')
         else:
-            raise Exception('Erro')          
+            raise Exception(f'API não acessada.')
             
