6fd52aa7c372369322e2d5b5e0e0eaa723bd8ec5
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-17 18:14:00 -0300

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ class CurrencyConverter:
         if from_currency == to_currency:
             return round(amount, 2)
         url = url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}"
-
         respo =  requests.get(url)
         if respo.status_code == 200:
             data = respo.json()
